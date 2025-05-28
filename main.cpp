e0c56bf980a036df302e7377aff1b24d5f0bfc9f
mele674 <marcmelendez3002@gmail.com>
2025-05-28 11:52:45 -0400

    Init

---
+++ main.cpp
@@ -0,0 +1,14 @@
+// program name goes here
+// your name goes here
+// any example usage or helpful command-line arguments go here
+
+#include <iostream>
+// Add any other header files here
+
+///////////////////////////////////////
+int main()
+{
+    // Work here
+}
+
+// Place any unused code here so that the instructor can follow your problem solving thoughts.
